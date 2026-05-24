492896dc922e9c54be8f531b469ee28c25412ccd
Anshul887 <anshulmankar887@gmail.com>
2026-05-24 13:33:20 +0530

    Create preprocess.py

---
+++ utils/preprocess.py
@@ -0,0 +1,23 @@
+import re
+import nltk
+
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+
+def preprocess(text):
+
+    text = text.lower()
+
+    text = re.sub(r'[^a-zA-Z ]', '', text)
+
+    words = text.split()
+
+    words = [
+        word for word in words
+        if word not in stop_words
+    ]
+
+    return " ".join(words)
